e488ce95055a9ded13228b0f4de0050ca181d8ba
Rodrigo Sarno <107512180+RodsSarno@users.noreply.github.com>
2025-05-03 20:50:45 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -3,8 +3,8 @@ import xmlrpc.client
 from classcliente import User
 
 server = xmlrpc.client.ServerProxy("http://localhost:8000")
-server = xmlrpc.client.ServerProxy("http://localhost:8001")
-server = xmlrpc.client.ServerProxy("http://localhost:8002")
+# server = xmlrpc.client.ServerProxy("http://localhost:8001")
+# server = xmlrpc.client.ServerProxy("http://localhost:8002")
 
 nome_usuario = input("Digite seu nome: ")
 
