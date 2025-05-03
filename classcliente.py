da4107e5daac8c566937aff95e3215b5db586a0c
Rodrigo Sarno <107512180+RodsSarno@users.noreply.github.com>
2025-05-03 19:37:30 -0300

    Update classcliente.py

--- classcliente.py
+++ classcliente.py
@@ -1,6 +1,7 @@
-Class User:
-    def __init__(self, nome_usuario, quantidade_pts, num_perg):
+# classcliente.py
+class User:
+    def __init__(self, nome_usuario, quantidade_pts=0, num_perg=0, resp_cliente=""):
         self.nome_usuario = nome_usuario
         self.quantidade_pts = quantidade_pts
         self.num_perg = num_perg
-
+        self.resp_cliente = resp_cliente
