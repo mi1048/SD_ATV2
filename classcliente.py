22b56102c884e14ae2d3d368c0e7ad760a83871f
Eric mnz <48776197+mi1048@users.noreply.github.com>
2025-05-03 21:06:28 -0300

    Update classcliente.py

--- classcliente.py
+++ classcliente.py
@@ -1,6 +1,5 @@
-# classcliente.py
 class User:
-    def __init__(self, nome_usuario, quantidade_pts=0, num_perg=0, resp_cliente=""):
+    def __init__(self, nome_usuario, quantidade_pts=0, num_perg=0, resp_cliente=None):
         self.nome_usuario = nome_usuario
         self.quantidade_pts = quantidade_pts
         self.num_perg = num_perg
