966d38c7e115ec5d113b70c78f424f9e7250aad0
Eric mnz <48776197+mi1048@users.noreply.github.com>
2025-05-03 21:40:28 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -6,12 +6,7 @@ server = xmlrpc.client.ServerProxy("http://localhost:8000")
 # server = xmlrpc.client.ServerProxy("http://localhost:8001")
 # server = xmlrpc.client.ServerProxy("http://localhost:8002")
 
-nome_usuario = input("Digite seu nome: ")
 
-quantidade_pts = 0
-num_perg = 0
-# cliente.py
-import xmlrpc.client
 
 server = xmlrpc.client.ServerProxy("http://localhost:8000")
-server.iniciar_quiz()
+print(server.iniciar_quiz())
