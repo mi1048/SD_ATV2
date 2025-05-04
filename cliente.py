5e909c45f1ae02d28fb57e738ce37ded897d7618
Eric mnz <48776197+mi1048@users.noreply.github.com>
2025-05-03 21:55:00 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,11 +2,20 @@
 import xmlrpc.client
 from classcliente import User
 
-server = xmlrpc.client.ServerProxy("http://localhost:8000")
-# server = xmlrpc.client.ServerProxy("http://localhost:8001")
-# server = xmlrpc.client.ServerProxy("http://localhost:8002")
+server = xmlrpc.client.ServerProxy("http://localhost:8000", allow_none=True)
 
+nome_usuario = input("Digite seu nome: ").strip()
+user = User(nome_usuario)
 
+while True:
+    # Envia o objeto como dicionário (para evitar problemas de serialização)
+    response = server.processar_interacao(user.__dict__)
 
-server = xmlrpc.client.ServerProxy("http://localhost:8000")
-print(server.iniciar_quiz())
+    print(response["mensagem"])
+
+    if response["fim"]:
+        break
+
+    # Solicita resposta do usuário, envia novamente
+    resposta = input("Sua resposta: ").strip().upper()
+    user.resp_cliente = resposta
